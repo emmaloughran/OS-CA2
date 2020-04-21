2184f280a84b5ea025b0bd3b4da751c86248448c
emmaloughran <emmaloughran2009@hotmail.co.uk>
2020-04-21 21:51:26 +0000

    moved the fill array to sys.c file

--- section-two/task_two.c
+++ section-two/task_two.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
-
+#include "./lib/sys.c"
 typedef struct {
 	uint8_t r;
 	uint8_t g;
@@ -9,7 +9,6 @@ typedef struct {
 }rgb;
 
 int append_0x();
-int assign_to_array(uint32_t num[25]);
 int  extract_green(uint32_t num[25]);
 
 int main()
@@ -52,37 +51,6 @@ int append_0x()
 	return 0;
 
 }
-
-
-int assign_to_array(uint32_t num[25])
-{
-
-	FILE *f;
-	//unsigned int num[80];
-	//uint32_t num[25];
-	int i=0;
-  int rv;
-  int num_values;
-
- 	f=fopen("removed_hash_prepended.txt","r");
-//	f=fopen("removed_hash_prepended.txt","r");
-//	f=fopen("hex_removed_hash.txt","r");
-  if (f==NULL){
-  	printf("unknown file\n");
-    return 1;
-	}
-
-	while (i <= 25) {
-  	rv = fscanf(f, "%x", &num[i]);
-
-		if (rv != 1)
-    	break;
-			i++;
-    }
-		fclose(f);
-    return 0;
-}
-
 
 int extract_green(uint32_t num[25])
 {

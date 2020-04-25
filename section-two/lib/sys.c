679377bebd2687fc193137ea26c25b991900ac2f
emmaloughran <emmaloughran2009@hotmail.co.uk>
2020-04-25 13:38:25 +0000

    added functions to sys.c that are mutual to task 2 and 3

--- section-two/lib/sys.c
+++ section-two/lib/sys.c
@@ -1,28 +1,38 @@
+//[6] reading from a file
  int assign_to_array(uint32_t num[25])
  {
 
    FILE *f;
-   //unsigned int num[80];
-   //uint32_t num[25];
 	int i=0;
 	int rv;
 	int num_values;
 	f=fopen("rm_hash.txt","r");
-//	f=system("cat hex.txt |  tail -25 | tr -d '#'");
+	while (i <= 25)
+	{
+		rv = fscanf(f, "%x", &num[i]);
+		if (rv != 1)
+			break;
+			i++;
+		}
 
-   if (f==NULL){
-     printf("unknown file\n");
-     return 1;
-   }
-
-   while (i <= 25) {
-     rv = fscanf(f, "%x", &num[i]);
-
-     if (rv != 1)
-       break;
-       i++;
-     }
-     fclose(f);
-     return 0;
+		fclose(f);
+		return 0;
  }
+
+//[7]
+int check_len(uint32_t num[25],int count[25])
+{
+
+	for(int i=0;i<25;i++)
+	{
+		do
+		{
+			count[i]++;
+			num[i] /= 10;
+		} while(num[i] != 0);
+	}
+
+	assign_to_array(num);
+	return 0;
+   }
 
